d1080eedf93d720c9328f7db3dc746085345d884
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-03-11 09:07:16 +0800

    修改优化算法时，自动弹出的处理方法

--- src/Contorl/Chipic.cpp
+++ src/Contorl/Chipic.cpp
@@ -346,7 +346,7 @@ bool Chipic::disposHintMessage(const Message& msg)
 		//如果为自动运行模式，则直接发送继续消息
 		if (getIsAuto())
 		{
-			sendMessage(108, 3, -1);
+			sendMessage(108, 3, -2);
 			break;
 		}
 		hintDailog.setText(msg.text);
