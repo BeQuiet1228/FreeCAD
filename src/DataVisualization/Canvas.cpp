282ca67a486616e22a9b9cae215f0f044ea0d3bd
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-03-12 10:53:30 +0800

    增加canvas和axis文件

---
+++ src/DataVisualization/Canvas.cpp
@@ -0,0 +1,12 @@
+#include "Canvas.h"
+Canvas::Canvas(QWidget* parent /*= 0*/)
+	:QWidget(parent)
+{
+
+}
+
+Canvas::~Canvas()
+{
+
+}
+
---
+++ src/DataVisualization/Canvas.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <QWidget>
+class Canvas :public QWidget{
+public:
+	Canvas(QWidget* parent = 0);
+	~Canvas();
+
+};
