ee8ccf7696f255e07d0b0f5dd65f8733ff80a052
白海鑫 <741216386@qq.com>
2021-03-18 00:17:38 +0800

    尝试提交

--- src/DataVisualization/Axis.cpp
+++ src/DataVisualization/Axis.cpp
@@ -10,3 +10,68 @@ Axis::~Axis()
 
 }
 
+void Axis::setAxisText(QString AxisUnitText, int fontsize){
+	Axisunit = AxisUnitText;//单位
+	Axisunitfontsize = fontsize;//字体大小
+}
+void Axis::setAxisRange(double min, double max){
+	axisvalrange.min = min;
+	axisvalrange.max = max;
+}
+void Axis::SetAxisNumber(int _Axisnumber){
+	Axisnumber = _Axisnumber;
+}
+void Axis::_update(){
+	//是否需要调整大小
+	QWidget* parent = (QWidget*)this->parent();
+	QSize parentsize = parent->size();//获取父级窗口大小
+	switch (mAxisstyle)
+	{
+	case Axisleft:
+	{
+		AxisRect.setLeft(0);
+		AxisRect.setTop(0);
+		AxisRect.setBottom(parent->height());
+		AxisRect.setRight(Axisunitfontsize + 50);
+	}
+		break;
+	case AxisRight:
+	{
+		AxisRect.setRight(parentsize.width());
+		AxisRect.setTop(0);
+		AxisRect.setBottom(parentsize.height());
+		AxisRect.setLeft(AxisRect.right() - (Axisunitfontsize + 50));
+	}
+		break;
+	case AxisTop:
+	{
+		AxisRect.setLeft(Axisunitfontsize + 50);
+		AxisRect.setRight(parentsize.width() - (Axisunitfontsize + 50));
+		AxisRect.setTop(0);
+		AxisRect.setBottom(AxisRect.top() + (Axisunitfontsize + 50));
+	}
+		break;
+	case AxisBottom:
+	{
+		AxisRect.setLeft(Axisunitfontsize + 50);
+		AxisRect.setRight(parentsize.width() - (Axisunitfontsize + 50));
+		AxisRect.setBottom(parentsize.height());
+		AxisRect.setTop(AxisRect.bottom() - (Axisunitfontsize + 50));
+	}
+		break;
+	default:
+		return;
+	}
+	//此处添加位置数据
+	update();
+}
+void Axis::setAxisStyle(Axisstyle _Axisstyle){
+	mAxisstyle = _Axisstyle;
+}
+void Axis::paintEvent(QPaintEvent* event){
+	//重绘
+
+
+
+
+}
--- src/DataVisualization/Axis.h
+++ src/DataVisualization/Axis.h
@@ -1,9 +1,40 @@
 #pragma  once
 #include <QWidget>
+#include<QString>
+typedef struct
+{
+	double min;
+	double max;
+}valrange;
+enum Axisstyle
+{
+	Axisleft,
+	AxisRight,
+	AxisTop,
+	AxisBottom
+};
+
 class Axis:public QWidget{
 public:
 	Axis(QWidget* parent = 0);
 	~Axis();
-
-
+public:
+	//添加功能函数
+	void setAxisText(QString, int fontsize);
+	void setAxisRange(double min,double max);
+	//设置大刻度个数
+	void SetAxisNumber(int);
+	//刷新
+	void _update();
+	//设置刻度风格
+	void setAxisStyle(Axisstyle);
+public:
+	void paintEvent(QPaintEvent* event);
+private:
+	unsigned int Axisnumber;//大刻度个数
+	QString Axisunit;//单位
+	int Axisunitfontsize;//单位字体大小
+	valrange axisvalrange;//刻度数值区间
+	QRectF AxisRect;//刻度所占窗体大小
+	Axisstyle mAxisstyle;//刻度风格
 };
