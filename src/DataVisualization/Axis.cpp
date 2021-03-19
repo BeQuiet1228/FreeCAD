fb69464065fa6f48fb52268dc3b9f503d0527b65
白海鑫 <741216386@qq.com>
2021-03-19 08:04:10 +0800

    Axis组件功能完成，待裁剪，分离计算

--- src/DataVisualization/Axis.cpp
+++ src/DataVisualization/Axis.cpp
@@ -1,81 +1,133 @@
-#include "Axis.h"
-Axis::Axis(QWidget* parent /*= 0*/)
-	:QWidget(parent)
+#include "axis.h"
+#include<QRect>
+#include<QSize>
+#include<QPainter>
+Axis::Axis(QWidget *parent) :
+    QWidget(parent)/*,horizontalAxis(0),verticalAxis(0),isstart(false)*/
 {
+    if(parent!=nullptr)
 
+    this->autoFillBackground();
 }
 
 Axis::~Axis()
 {
 
 }
-
-void Axis::setAxisText(QString AxisUnitText, int fontsize){
-	Axisunit = AxisUnitText;//单位
-	Axisunitfontsize = fontsize;//字体大小
-}
-void Axis::setAxisRange(double min, double max){
-	axisvalrange.min = min;
-	axisvalrange.max = max;
-}
-void Axis::SetAxisNumber(int _Axisnumber){
-	Axisnumber = _Axisnumber;
-}
-void Axis::_update(){
-	//是否需要调整大小
-	QWidget* parent = (QWidget*)this->parent();
-	QSize parentsize = parent->size();//获取父级窗口大小
-	switch (mAxisstyle)
-	{
-	case Axisleft:
-	{
-		AxisRect.setLeft(0);
-		AxisRect.setTop(0);
-		AxisRect.setBottom(parent->height());
-		AxisRect.setRight(Axisunitfontsize + 50);
-	}
-		break;
-	case AxisRight:
-	{
-		AxisRect.setRight(parentsize.width());
-		AxisRect.setTop(0);
-		AxisRect.setBottom(parentsize.height());
-		AxisRect.setLeft(AxisRect.right() - (Axisunitfontsize + 50));
-	}
-		break;
-	case AxisTop:
-	{
-		AxisRect.setLeft(Axisunitfontsize + 50);
-		AxisRect.setRight(parentsize.width() - (Axisunitfontsize + 50));
-		AxisRect.setTop(0);
-		AxisRect.setBottom(AxisRect.top() + (Axisunitfontsize + 50));
-	}
-		break;
-	case AxisBottom:
-	{
-		AxisRect.setLeft(Axisunitfontsize + 50);
-		AxisRect.setRight(parentsize.width() - (Axisunitfontsize + 50));
-		AxisRect.setBottom(parentsize.height());
-		AxisRect.setTop(AxisRect.bottom() - (Axisunitfontsize + 50));
-	}
-		break;
-	default:
-		return;
-	}
-	//此处添加位置数据
-	update();
-}
-void Axis::setAxisStyle(Axisstyle _Axisstyle){
-	mAxisstyle = _Axisstyle;
-}
-void Axis::paintEvent(QPaintEvent* event){
-	//重绘
- //获取窗口的大小
+void Axis::paintEvent(QPaintEvent* event)
+{
+    //获取窗口的大小
     QSize clientsize=this->size();
     //获取客户区的长宽
     qreal width=clientsize.width();
     qreal height=clientsize.height();
     QPainter mPainter(this);
+
+    switch(mAxisstyle)
+    {
+    case Axisleft:
+    {
+        //画线段
+        mPainter.drawLine(AxisRect.right()-1,AxisRect.top()+1,AxisRect.right()-1,AxisRect.bottom()-1);
+        //画刻度
+        //间隔
+        qreal Axisinterval=(AxisRect.bottom()-AxisRect.top()-2)/(Axisnumber*5);
+        QPointF startPointf=QPointF(AxisRect.right()-1,AxisRect.bottom()-1);
+        QPointF nextPointf=startPointf;
+        qreal AxisRangeinterval=(axisvalrange.max-axisvalrange.min)/(Axisnumber*5);
+        for(auto i=0;i<=Axisnumber*5;i++)
+        {
+            nextPointf.setY(startPointf.y()-(Axisinterval*i));
+            if(i%5==0)
+            {
+                mPainter.drawLine(nextPointf.x(),nextPointf.y(),nextPointf.x()-10,nextPointf.y());
+                qreal curval=axisvalrange.min+i*AxisRangeinterval;
+                QString curvalstr=QString("%1").arg(curval);
+                mPainter.drawText(QPointF(nextPointf.x()-10-((curvalstr.length()+1)*5),nextPointf.y()+5),curvalstr);
+            }
+            else
+            {
+                mPainter.drawLine(nextPointf.x(),nextPointf.y(),nextPointf.x()-6,nextPointf.y());
+            }
+        }
+    }
+        break;
+    case AxisRight:
+    {
+
+       mPainter.drawLine(AxisRect.left()+1,AxisRect.top()+1,AxisRect.left()+1,AxisRect.bottom()-1);
+       qreal Axisinterval=(AxisRect.bottom()-AxisRect.top()-2)/(Axisnumber*5);
+       QPointF startPointf=QPointF(AxisRect.left()+1,AxisRect.bottom()-1);
+       QPointF nextPointf=startPointf;
+        qreal AxisRangeinterval=(axisvalrange.max-axisvalrange.min)/(Axisnumber*5);
+       for(auto i=0;i<=Axisnumber*5;i++)
+       {
+           nextPointf.setY(startPointf.y()-(Axisinterval*i));
+           if(i%5==0)
+           {
+               mPainter.drawLine(nextPointf.x(),nextPointf.y(),nextPointf.x()+10,nextPointf.y());
+               qreal curval=axisvalrange.min+i*AxisRangeinterval;
+               QString curvalstr=QString("%1").arg(curval);
+               mPainter.drawText(QPointF(nextPointf.x()+10,nextPointf.y()+5),curvalstr);
+           }
+           else
+           {
+               mPainter.drawLine(nextPointf.x(),nextPointf.y(),nextPointf.x()+6,nextPointf.y());
+           }
+       }
+    }
+        break;
+    case AxisTop:
+    {
+        mPainter.drawLine(AxisRect.left()+1,AxisRect.bottom()-1,AxisRect.right()-1,AxisRect.bottom()-1);
+        qreal Axisinterval=(AxisRect.right()-AxisRect.left()-2)/(Axisnumber*5);
+        QPointF startpointf=QPointF(AxisRect.left()+1,AxisRect.bottom()-1);
+        //获取刻度间隔
+        qreal AxisRangeinterval=(axisvalrange.max-axisvalrange.min)/(Axisnumber*5);
+        QPointF nextpointf=startpointf;
+        for(auto i=0;i<=Axisnumber*5;i++)
+        {
+            nextpointf.setX(startpointf.x()+Axisinterval*i);
+            if(i%5==0)
+            {
+                mPainter.drawLine(nextpointf.x(),nextpointf.y(),nextpointf.x(),nextpointf.y()-10);
+                qreal curval=axisvalrange.min+i*AxisRangeinterval;
+                QString curvalstr=QString("%1").arg(curval);
+                mPainter.drawText(QPointF(nextpointf.x()-curvalstr.length()*5/2,nextpointf.y()-10),curvalstr);
+            }
+            else
+            {
+                mPainter.drawLine(nextpointf.x(),nextpointf.y(),nextpointf.x(),nextpointf.y()-6);
+            }
+        }
+    }
+        break;
+    case AxisBottom:
+    {
+        mPainter.drawLine(AxisRect.left()+1,AxisRect.top()+1,AxisRect.right()-1,AxisRect.top()+1);
+        qreal Axisinterval=(AxisRect.right()-AxisRect.left()-2)/(Axisnumber*5);
+        QPointF startpointf=QPointF(AxisRect.left()+1,AxisRect.top()+1);
+        QPointF nextpointf=startpointf;
+         qreal AxisRangeinterval=(axisvalrange.max-axisvalrange.min)/(Axisnumber*5);
+        for(auto i=0;i<=Axisnumber*5;i++)
+        {
+            nextpointf.setX(startpointf.x()+Axisinterval*i);
+            if(i%5==0)
+            {
+                mPainter.drawLine(nextpointf.x(),nextpointf.y(),nextpointf.x(),nextpointf.y()+10);
+                qreal curval=axisvalrange.min+i*AxisRangeinterval;
+                QString curvalstr=QString("%1").arg(curval);
+                mPainter.drawText(QPointF(nextpointf.x()-curvalstr.length()*5/2,nextpointf.y()+20),curvalstr);
+            }
+            else
+            {
+                mPainter.drawLine(nextpointf.x(),nextpointf.y(),nextpointf.x(),nextpointf.y()+6);
+            }
+
+        }
+    }
+        break;
+    }
     switch (mAxisstyle) {
     case Axisleft:
         {
@@ -153,13 +205,12 @@ void Axis::paintEvent(QPaintEvent* event){
     }
         break;
     }
+//开始绘制刻度
 
-    if(0==Axisnumber)
-        return;
+
+
     //绘制坐标单位
-    int length=Axisunit.length();
     //获取坐标的区间
-
 //    //画横坐标刻度
 //    //长刻度为10，短刻度为6
 //    QVector<qreal> horizontalAxislist;
@@ -233,7 +284,61 @@ void Axis::paintEvent(QPaintEvent* event){
 //    verticalvalrange.min=min;
 //    verticalvalrange.max=max;
 //}
-
-
-
+void Axis::_update()
+{
+    //是否需要调整大小
+//    QWidget* parent=(QWidget*)this->parent();
+//    QSize parentsize=parent->size();//获取父级窗口大小
+    QSize parentsize=/*QSize(800,600)*/this->size();
+    switch(mAxisstyle)
+    {
+    case Axisleft:
+    {
+        AxisRect.setLeft(0);
+        AxisRect.setTop(Axisunitfontsize+50);
+        AxisRect.setBottom(parentsize.height()-(Axisunitfontsize+50));
+        AxisRect.setRight(Axisunitfontsize+50);
+    }
+        break;
+    case AxisRight:
+    {
+        AxisRect.setRight(parentsize.width());
+        AxisRect.setTop(Axisunitfontsize+50);
+        AxisRect.setBottom(parentsize.height()-(Axisunitfontsize+50));
+        AxisRect.setLeft(AxisRect.right()-(Axisunitfontsize+50));
+    }
+        break;
+    case AxisTop:
+    {
+        AxisRect.setLeft(Axisunitfontsize+50);
+        AxisRect.setRight(parentsize.width()-(Axisunitfontsize+50));
+        AxisRect.setTop(0);
+        AxisRect.setBottom(AxisRect.top()+(Axisunitfontsize+50));
+    }
+        break;
+    case AxisBottom:
+    {
+        AxisRect.setLeft(Axisunitfontsize+50);
+        AxisRect.setRight(parentsize.width()-(Axisunitfontsize+50));
+        AxisRect.setBottom(parentsize.height());
+        AxisRect.setTop(AxisRect.bottom()-(Axisunitfontsize+50));
+    }
+        break;
+    }
+    update();
+}
+void Axis::setAxixStyle(Axisstyle _Axisstyle)
+{
+    mAxisstyle=_Axisstyle;
+}
+void Axis::setAxisText(QString AxisUnitText,int fontsize){
+    Axisunit=AxisUnitText;//单位
+    Axisunitfontsize=fontsize;//字体大小
+}
+void Axis::setAxisRange(double min,double max){
+    axisvalrange.min=min;
+    axisvalrange.max=max;
+}
+void Axis::SetAxisNumber(int _Axisnumber){
+    Axisnumber=_Axisnumber;
 }
--- src/DataVisualization/Axis.h
+++ src/DataVisualization/Axis.h
@@ -1,40 +1,47 @@
-#pragma  once
+#pragma once
+
+#ifndef AXIS_H
+#define AXIS_H
+
 #include <QWidget>
-#include<QString>
+#include<QVector>
 typedef struct
 {
-	double min;
-	double max;
+    double min;
+    double max;
 }valrange;
 enum Axisstyle
 {
-	Axisleft,
-	AxisRight,
-	AxisTop,
-	AxisBottom
+    Axisleft,
+    AxisRight,
+    AxisTop,
+    AxisBottom
+};
+class Axis : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Axis(QWidget *parent = nullptr);
+    ~Axis();
+private:
+public:
+    //添加功能函数
+    void setAxisText(QString,int fontsize);
+    void setAxisRange(double min,double max);
+    //设置大刻度个数
+    void SetAxisNumber(int);
+    void _update();
+    void setAxixStyle(Axisstyle);
+public:
+    void paintEvent(QPaintEvent* event);
+private:
+    bool isstart;
+    unsigned int Axisnumber;//大刻度个数
+    QString Axisunit;//单位
+    int Axisunitfontsize;//单位字体大小
+    valrange axisvalrange;//刻度数值区间
+    QRectF AxisRect;
+    Axisstyle mAxisstyle;
 };
 
-class Axis:public QWidget{
-public:
-	Axis(QWidget* parent = 0);
-	~Axis();
-public:
-	//添加功能函数
-	void setAxisText(QString, int fontsize);
-	void setAxisRange(double min,double max);
-	//设置大刻度个数
-	void SetAxisNumber(int);
-	//刷新
-	void _update();
-	//设置刻度风格
-	void setAxisStyle(Axisstyle);
-public:
-	void paintEvent(QPaintEvent* event);
-private:
-	unsigned int Axisnumber;//大刻度个数
-	QString Axisunit;//单位
-	int Axisunitfontsize;//单位字体大小
-	valrange axisvalrange;//刻度数值区间
-	QRectF AxisRect;//刻度所占窗体大小
-	Axisstyle mAxisstyle;//刻度风格
-};
+#endif // AXIS_H
