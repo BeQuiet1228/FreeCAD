3ec1f4060c8be328ab67d65f37c7647293afae73
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-04-01 08:58:13 +0800

    增加运行类型消息

--- src/Contorl/Chipic.cpp
+++ src/Contorl/Chipic.cpp
@@ -346,13 +346,7 @@ bool Chipic::disposHintMessage(const Message& msg)
 		//如果为自动运行模式，则直接发送继续消息
 		if (getIsAuto())
 		{
-			int i = 100;
-			while (i > 0)
-			{
-				i--;
-				sendMessage(108, 3, -2);
-			}
-			
+			sendMessage(108, 3, -2);
 			break;
 		}
 		hintDailog.setText(msg.text);
