4c83b4a909298dd97e4e1f5742ef899ac66f27f4
白海鑫 <741216386@qq.com>
2021-04-02 15:20:11 +0800

    增加矢量图相关项目文件

--- src/HDF5Reader/hdf5io.cpp
+++ src/HDF5Reader/hdf5io.cpp
@@ -35,7 +35,7 @@ void Hdf5IO::setFilePath(const std::string& path)
 	QString temp = QString::fromUtf8(path.c_str());
 	qDebug() << temp;
 	//std::string newPath = gbk->fromUnicode(temp).data();
-	std::string newPath = "F:/wdtproject/PICGUI/TEMP(1).H5";
+	std::string newPath = "F:/wdtproject/PICGUI/MILO_C_2.h5";
 	deleteH5File();
 	Hdf5File = new H5File(newPath, H5F_ACC_RDWR);
 }
