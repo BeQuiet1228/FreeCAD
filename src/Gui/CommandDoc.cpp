c70e41e65f6ee9df7ca8db5ea09a68735d7bc2c5
白海鑫 <741216386@qq.com>
2021-04-23 15:19:05 +0800

    树控件添加完成

--- src/Gui/CommandDoc.cpp
+++ src/Gui/CommandDoc.cpp
@@ -114,12 +114,13 @@ void StdCmdOpen::activated(int iMsg)
         filetypes.insert(filetypes.begin(), "FCStd");
     }
 
-	it = std::find(filetypes.begin(), filetypes.end(), "h5");
+	filetypes.insert(filetypes.begin() + 1, "h5");
+	/*it = std::find(filetypes.begin(), filetypes.end(), "h5");
 	if (it!=filetypes.end())
 	{
 		filetypes.erase(it);
 		filetypes.insert(filetypes.begin() + 1, "h5");
-	}
+	}*/
     for (it=filetypes.begin();it != filetypes.end();++it) {
         formatList += QLatin1String(" *.");
         formatList += QLatin1String(it->c_str());
