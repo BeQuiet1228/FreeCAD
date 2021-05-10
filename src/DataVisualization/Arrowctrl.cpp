b76a7c70175219b395433554590056a590017c58
白海鑫 <741216386@qq.com>
2021-05-10 18:00:08 +0800

    暂存

---
+++ src/DataVisualization/Arrowctrl.cpp
@@ -0,0 +1,25 @@
+#include "Arrowctrl.h"
+std::string pngresource[] = { ":/Arrow/arrow1.png" };
+ArrowCtrl::ArrowCtrl(Direction direction, QWidget* parent) :QWidget(parent), mdirection(direction)
+{
+
+}
+ArrowCtrl::~ArrowCtrl(){
+
+}
+void ArrowCtrl::paintEvent(QPaintEvent * event)
+{
+	printf("重绘\n");
+}
+void ArrowCtrl::mouseMoveEvent(QMouseEvent* event)
+{
+	printf("鼠标移动\n");
+}
+void ArrowCtrl::mousePressEvent(QMouseEvent* event)
+{
+	printf("鼠标按下");
+}
+void ArrowCtrl::mouseReleaseEvent(QMouseEvent* event)
+{
+	printf("鼠标释放");
+}
