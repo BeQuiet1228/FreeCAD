55a2e2075e795f21d9f48200bcf10efb6de45b8c
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-05-12 18:04:05 +0800

    解决空间变化图画不了的情况

--- src/DataVisualization/TimeData.cpp
+++ src/DataVisualization/TimeData.cpp
@@ -81,8 +81,9 @@ QPointF TimeData::getPointHard(const unsigned int& index)
 */
 unsigned int TimeData::findIndexFromXValueL(const float& x)
 {
-	double step = (getXRang().max - getXRang().min) / getPointSize();
-	unsigned int index = x / step;
+	auto xr = getXRang();
+	double step = xr.length() / getPointSize();
+	unsigned int index = (x - xr.min )/ step;
 	//如果索引超出范围则返回0
 	if (index > getPointSize())
 		return getPointSize();
