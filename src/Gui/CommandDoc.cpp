e76d4d4218ce0ab7a6c40007ab1ba061944cb7c7
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-05-21 09:36:53 +0800

    并行运行时增加路径添加

--- src/Gui/CommandDoc.cpp
+++ src/Gui/CommandDoc.cpp
@@ -2012,6 +2012,25 @@ void StdCmdParalleRun::activated(int iMsg)
 	//调用保存
 	doCommand(Command::Gui, "Gui.SendMsgToActiveView(\"Save\")");
 	auto contorl = ContorlInterface::GetInstance();
+
+	//设置运行路
+	auto guiDoc = Gui::Application::Instance->activeDocument();
+	auto appDoc = guiDoc->getDocument();
+	std::string path = appDoc->FileName.getValue();
+	if (appDoc->classID == 2)
+	{
+		QString temp = QString::fromUtf8(path.c_str());
+		temp = temp.left(temp.length() - 6) + QString::fromLocal8Bit(".m3d");
+		path = temp.toStdString();
+	}
+	else if (appDoc->classID == 3) {
+		QString temp = QString::fromUtf8(path.c_str());
+		temp = temp.left(temp.length() - 9) + QString::fromLocal8Bit(".m2d");
+		path = temp.toStdString();
+	}
+	std::cerr << path << std::endl;
+	contorl->setM3dPath(path);
+
 	contorl->buttonClicked(1);
 }
 bool StdCmdParalleRun::isActive(void)
