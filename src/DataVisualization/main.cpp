9c66e3d5cbec5760b7eff67a96dab482bba46667
白海鑫 <741216386@qq.com>
2021-05-26 11:30:45 +0800

    暂存

--- src/DataVisualization/main.cpp
+++ src/DataVisualization/main.cpp
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	CanvasItem::registerMetaTye();
 #if 1
-	//std::string path = "D:/H5/MILO.h5";
-	std::string path = "D:/MILO_D(1).h5";
+	std::string path = "D:/H5/MILO.h5";
+	//std::string path = "D:/MILO_D(1).h5";
 	//std::string path = "D:/MILO_P.h5";
 	//std::string path = "D:/MILO_C.h5";
 	Plot p;
