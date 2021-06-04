da3b903eb40f28f6ab8b2ef8fcab098b4dbcbba4
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-06-04 09:39:26 +0800

    暂时增加colormap的读取，这里没有考虑线程安全，后期再修改

--- src/DataVisualization/ContourRender.cpp
+++ src/DataVisualization/ContourRender.cpp
@@ -8,12 +8,12 @@
 #include <qmath.h>
 #include "CustomConfig.h"
 #include "C_encoding.h"
+#include "ConfigWidget.h"
 ContourRender::ContourRender(std::shared_ptr<ContourData> data)
 	:Renderer(std::dynamic_pointer_cast<Data>(data)),contourLevelsMod(EQUAL_DIFFERENCE)
 	,contourLevel(10)
 {
 	setRenderThreadCount(0);
-	setColorMap(new ColorMap);
 
 	setDisplayMode(DisplayMode::ImageMode, true);
 	setDisplayMode(DisplayMode::ContourMode, true);
@@ -26,6 +26,8 @@ ContourRender::~ContourRender()
 
 bool ContourRender::drawImage()
 {
+	setColorMap(ConfigWidget::getQwtLinearColorMap());
+
 	QwtScaleMap xmap, ymap;
 	xmap.setPaintInterval(0, this->getSize().width());
 	xmap.setScaleInterval(getXRang().min, getXRang().max);
