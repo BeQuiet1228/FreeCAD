be097604bb972c79c51ed30c8656de4cc7e2bff3
oldmemorise <741216386@qq.com>
2021-07-02 15:02:23 +0800

    1

--- src/DataVisualization/Axis.cpp
+++ src/DataVisualization/Axis.cpp
@@ -1,6 +1,8 @@
 #include "axis.h"
 #include<QGridLayout>
 #include "qwt/qwt_scale_draw.h"
+#include "qwt/qwt_scale_widget.h"
+#include"qwt/qwt_scale_engine.h"
 Axis::Axis(QWidget* parent):QWidget(parent)
 {
 	//设置默认参数
@@ -10,6 +12,7 @@ Axis::Axis(QWidget* parent):QWidget(parent)
 	axisvalrange.min = 0.0f;
 	axisvalrange.max = 100.0f;
 	mqgridlayout = new QGridLayout;
+	mqgridlayout->setSpacing(0);
 	this->setLayout(mqgridlayout);
 }
 Axis::~Axis()
@@ -44,26 +47,47 @@ void Axis::loadconfig()
 void Axis::_update()
 {
 	int widgetCount = mqgridlayout->count();
-	for (int index=widgetCount;index>=0;index--)
+	QwtScaleWidget* mQwtScaleWidget = nullptr;
+	for (int index=widgetCount-1;index>=0;index--)
 	{
-		
+		mQwtScaleWidget = dynamic_cast<QwtScaleWidget*>(mqgridlayout->itemAt(index)->widget());
+		if (mQwtScaleWidget)
+			break;
 	}
+	if (!mQwtScaleWidget)
+	{
+		mQwtScaleWidget = new QwtScaleWidget();
+	}
+	mQwtScaleWidget->hide();
 	switch (mAxisstyle)
 	{
 	case Axisleft:
 	{
+		//mQwtScaleWidget = new QwtScaleWidget(QwtScaleDraw::LeftScale);
+		mQwtScaleWidget->setAlignment(QwtScaleDraw::LeftScale);
 	}break;
 	case AxisRight:
 	{
+		mQwtScaleWidget->setAlignment(QwtScaleDraw::RightScale);
+		//mQwtScaleWidget = new QwtScaleWidget(QwtScaleDraw::RightScale);
 	}break;
 	case AxisTop:
 	{
+		mQwtScaleWidget->setAlignment(QwtScaleDraw::TopScale);
+		//mQwtScaleWidget = new QwtScaleWidget(QwtScaleDraw::TopScale);
 	}break;
 	case AxisBottom:
 	{
-		QwtScaleDraw* newScale = new QwtScaleDraw();
-		
+		mQwtScaleWidget->setAlignment(QwtScaleDraw::BottomScale);
+		//mQwtScaleWidget = new QwtScaleWidget(QwtScaleDraw::BottomScale);
+	
 	}break;
 	}
+	mQwtScaleWidget->setColorBarEnabled(true);
+	QwtLinearScaleEngine * mQwtLinearScaleEngine = new QwtLinearScaleEngine;
+	mQwtScaleWidget->setScaleDiv(mQwtLinearScaleEngine->divideScale(axisvalrange.min, axisvalrange.max, AxisNum, 5));
+	mqgridlayout->addWidget(mQwtScaleWidget, 0, 0);
+	//mQwtScaleWidget->setShown(false);
+	mQwtScaleWidget->show();
 }
 #include "moc_Axis.cpp"
