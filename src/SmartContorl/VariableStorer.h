d9806eaa77d20d5343bd8f08ae28d8e6461b2c36
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-07-06 17:29:54 +0800

    增加了一些接口

---
+++ src/SmartContorl/VariableStorer.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "xml/pugixml.hpp"
+class VariableStorer {
+public:
+	VariableStorer();
+	~VariableStorer();
+
+public:
+	bool loadFile(const std::string& path);
+	void closeFile();
+	bool fileIsLoad();
+	bool saveFile(const std::string path);
+	bool saveFile();
+	bool variableIsExsit(const std::string& name);
+	void addVariable(const std::string& name, const std::string& var);
+	
+	//获取数据
+public:
+	std::string getVariableToString(const std::string& name);
+	int getVariableToInt(const std::string& name);
+	double getVariableToDouble(const std::string& name);
+	long long getVariableToLLong(const std::string& name);
+	float getVariableToFloat(const std::string& name);
+private:
+	//xml 对象
+	pugi::xml_document* xmlDocument;
+	//文件路径
+	std::string filePath;
+private:
+	void deleteXmlDocument();
+	pugi::xml_attribute getVariableAtrribute(const std::string& name);
+};
