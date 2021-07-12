0fe14a505c4602678f6769dc6ee318f8213eb470
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-07-12 10:01:11 +0800

    完成断点续算

--- src/SmartContorl/SmartContorlUI.cpp
+++ src/SmartContorl/SmartContorlUI.cpp
@@ -343,10 +343,12 @@ QString SmartContorlUI::replaceVariate()
 	}
 	QString text;
 #ifdef SMART_EXE
-	QFile file(QString::fromLocal8Bit("E:/工作/优化算法/脚本.lua"));
-	file.open(QIODevice::ReadOnly);
-	text = file.readAll();
-	file.close();
+	{
+		QFile file(QString::fromLocal8Bit("E:/工作/优化算法/脚本.lua"));
+		file.open(QIODevice::ReadOnly);
+		text = file.readAll();
+		file.close();
+	}
 #else
 	text = this->ui->textEdit->toPlainText();
 #endif // SMART_EXE
@@ -378,7 +380,23 @@ QString SmartContorlUI::replaceVariate()
 	config += temp;
 	temp = QString("accuracy = %1/100;\n").arg(this->ui->lineEditAccuracy->text());
 	config += temp;
+
+	//是否按照上次优化数据计息
+	if (this->ui->checkBoxContinue->checkState() == Qt::Checked)
+		config += "continue = true;\n";
+	else
+		config += "continue = false;\n";
+
 	text = config + text;
+
+#ifdef SMART_EXE //输出拼接之后的脚本
+	{
+		QFile file(QString::fromLocal8Bit("./SmartControl.lua"));
+		file.open(QIODevice::ReadWrite);
+		file.write(text.toUtf8());
+		file.close();
+	}
+#endif // SMART_EXE
 	return text;
 }
 
@@ -400,6 +418,7 @@ void SmartContorlUI::saveParameterXml()
 	configNode.append_attribute("C1") = ui->lineEditC1->text().toStdString().c_str();
 	configNode.append_attribute("C2") = ui->lineEditC2->text().toStdString().c_str();
 	configNode.append_attribute("Omega") = ui->lineEditOmega->text().toStdString().c_str();
+	configNode.append_attribute("Continue") = ui->checkBoxContinue->checkState();
 
 	for (auto i = variateDatas.begin(); i != variateDatas.end(); i++)
 	{
@@ -447,7 +466,7 @@ void SmartContorlUI::loadParameterXml()
 	ui->lineEditC1->setText(QString::number(configNode.attribute("C1").as_double()));
 	ui->lineEditC2->setText(QString::number(configNode.attribute("C2").as_double()));
 	ui->lineEditOmega->setText(QString::number(configNode.attribute("Omega").as_double()));
-
+	ui->checkBoxContinue->setCheckState(Qt::CheckState(configNode.attribute("Continue").as_int()));
 	for (auto iter = parNode.begin(); iter != parNode.end(); iter++)
 	{
 		std::shared_ptr<VariateData> data;
