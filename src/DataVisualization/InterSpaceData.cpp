c228a25f60b54a118b6c98e6f3cce1e2aef62af0
DESKTOP-MGAHTTR\Administrator <anjztb@qq.com>
2021-08-23 11:08:39 +0800

    增加空间变化如图对不均匀空间的处理

--- src/DataVisualization/InterSpaceData.cpp
+++ src/DataVisualization/InterSpaceData.cpp
@@ -6,3 +6,24 @@ InterspaceData::InterspaceData(Hdf5Data& h5Data, const RunMod& mod /*= SINGLE_TH
 
 }
 
+unsigned int InterspaceData::findIndexFromXValueL(const float& x)
+{
+	auto xr = getXRang();
+	//如果范围小于最小值，那么直接返回第一个数值的索引
+	if (x < xr.min)
+		return 0;
+	if (x > xr.max)
+		return getPointSize() - 1;
+
+	unsigned int fm, am;
+	fm = 0;
+	am = getPointSize() - 1;
+	unsigned int index;
+
+	while ((am - fm) > 1) {
+		index = (am - fm) / 2;
+		getPoint(index + fm).x() > x ? am = index + fm : fm += index;
+	}
+	return fm;
+}
+
