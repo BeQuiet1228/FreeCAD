33fb905bfd1aac2c616851a2a541249cdf9ac1cc
DESKTOP-3TSNFHS\Administrator <494791436@qq.com>
2021-09-24 14:06:32 +0800

    点击确定更新模型

--- src/Gui/MyParameter.cpp
+++ src/Gui/MyParameter.cpp
@@ -821,13 +821,13 @@ void MyParameter::changeParamName() {
 void MyParameter::updateM3D() {
     if (GetApplication().getActiveDocument()->classID == 2) {
         Base::InterpreterSingleton python;
+        python.runString("FreeCADGui.runCommand('Refresh_3D')");
+        python.runString("FreeCADGui.runCommand('UpdateBooleanCommand_3D')");
         python.runString("FreeCADGui.runCommand('CreateM3D_new')");
-        python.runString("FreeCADGui.runCommand('Std_My_Parameter')");
     }
     else if (GetApplication().getActiveDocument()->classID == 3) {
         Base::InterpreterSingleton python;
         python.runString("FreeCADGui.runCommand('CreateM2D')");
-        python.runString("FreeCADGui.runCommand('Std_My_Parameter')");
     }
 }
 
