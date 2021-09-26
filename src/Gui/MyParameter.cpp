6a1232181c779618e20c3b0a80b9f3826e1b0b45
DESKTOP-O63ENCC\Administrator <374747519@qq.com>
2021-09-26 17:36:22 +0800

    单击param后查看到的参数value在属性栏中定义为不可编辑

--- src/Gui/MyParameter.cpp
+++ src/Gui/MyParameter.cpp
@@ -825,6 +825,7 @@ void MyParameter::updateM3D() {
         python.runString("FreeCADGui.runCommand('Refresh_3D')");
         python.runString("FreeCADGui.runCommand('UpdateBooleanCommand_3D')");
         python.runString("FreeCADGui.runCommand('CreateM3D_new')");
+        python.runString("FreeCADGui.runCommand('SingleClickParaCommand')");
         python.runString("FreeCADGui.runCommand('Std_My_Parameter')");
     }
     else if (GetApplication().getActiveDocument()->classID == 3) {
