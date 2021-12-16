5a9d47eae7831b0e9a3d7e54d6df28248095757f
ct <anjztb@qq.com>
2021-12-16 09:38:17 +0800

    更改DataVisulization 中的Data类保存接口

--- src/DataVisualization/Data.h
+++ src/DataVisualization/Data.h
@@ -73,7 +73,7 @@ public:
 public:
 	Data(Hdf5Data& h5Data ,const RunMod& mod = SINGLE_THREAD);
 	virtual ~Data();
-	void saveAs(std::string path, SaveMod mod = PUSHBACK);
+	virtual void saveAs(std::string path, SaveMod mod = PUSHBACK);
 protected:
 	//h5文件数据
 	Hdf5Data h5Data;
