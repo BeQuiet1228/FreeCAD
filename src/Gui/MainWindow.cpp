01ff1a277d0443350fd840e6a8fa966014be523c
ct <anjztb@qq.com>
2023-10-08 15:33:54 +0800

    修改order之后不布尔运算

--- src/Gui/MainWindow.cpp
+++ src/Gui/MainWindow.cpp
@@ -1772,6 +1772,8 @@ void MainWindow::insertFromMimeData (const QMimeData * mimeData)
             Gui::Document* gui = Application::Instance->getDocument(doc);
             if (gui)
                 gui->addRootObjectsToGroup(newObj, grp.front());
+            //复制之后刷新布尔运算
+            Gui::Application::Instance->commandManager().runCommandByName("UpdateBooleanCommand_3D");
         }
         doc->commitTransaction();
     }
--- src/Mod/Model3D/Tools/ObjectTools.py
+++ src/Mod/Model3D/Tools/ObjectTools.py
@@ -362,6 +362,8 @@ def updateWhenOrderChanged(obj, lastOrder, newOrder):
     """
     当Order发生变化时自动调整所有obj的Order
     """
+    ordered_list = getOrderedObjects()  # 已经排序的obj的list
+    objNumbers = len(ordered_list)  # 当前所有obj的数量
     # 首先判断newOrder, lastOrder均在合理范围内
     if 0 <= newOrder < objNumbers:
         pass
@@ -369,8 +371,6 @@ def updateWhenOrderChanged(obj, lastOrder, newOrder):
         FreeCAD.Console.PrintError("error:传入的order:  " + str(newOrder) + "不在合理范围内\n")
         newOrder = objNumbers - 1
 
-    ordered_list = getOrderedObjects()  # 已经排序的obj的list
-    objNumbers = len(ordered_list)  # 当前所有obj的数量
     tempOrder = 0
     for i in ordered_list:
         if i == obj:
@@ -389,6 +389,8 @@ def updateWhenOrderChanged(obj, lastOrder, newOrder):
                 continue
         i.Order = tempOrder
         tempOrder = tempOrder +1
+    if obj.Order != newOrder:
+        obj.Order = newOrder
     return
     if objNumbers == 1:
         obj.Order = 0
